271e6309180d802a5e886f0945d33ea930c82a26
wasiakaa <aleksandra.wasiak.stud@pw.edu.pl>
2023-04-24 21:32:16 +0200

    Dodałam komentarze do algorytmu greedy

--- algorytm_zachlanny.py
+++ algorytm_zachlanny.py
@@ -1,19 +1,19 @@
 def greedy(G, k):
-    n = len(G)
-    col = [0] * n
-    col_numb = [0] * n
+    n = len(G)  # liczba wierzchołków grafu G
+    col = [0] * n  # tu będziemy zapisywać kolory kolejnych wierzchołków
+    col_numb = [0] * n  # tu zliczamy ile jest wierzchołków w danym kolorze
     for i in range(1, n+1):
         for j in range(1, n+1):
-            if col_numb[j-1] < k:
-                if col[i-1] == 0:
-                    checksum = 0
+            if col_numb[j-1] < k:  # sprawdzamy, czy kolor j nie został jeszcze użyty k razy
+                if col[i-1] == 0:  # sprawdzamy, czy wierzchołek i nie został jeszcze pokolorowany
+                    checksum = 0  # warunek sprawdza, czy kolor j nie jest zajęty przez sąsiadów
                     for v in G[i]:
-                        if v < i:
-                            if col[v-1] != j:
-                                checksum = checksum
+                        if v < i:  # pokolorowani sąsiedzi wierzchołka i
+                            if col[v-1] != j:          # sprawdzamy czy sąsiad v wierzchołka i
+                                checksum = checksum    # nie ma koloru j
                             else:
                                 checksum = checksum + 1
                     if checksum == 0:
-                        col[i-1] = j
-                        col_numb[j-1] = col_numb[j-1] + 1
+                        col[i-1] = j  # kolorujemy wierzchołek i na kolor j
+                        col_numb[j-1] = col_numb[j-1] + 1  # zwiększamy l. wierzchołków w kolorze j
     return col
