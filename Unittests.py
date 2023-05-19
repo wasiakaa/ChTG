caa45adad15fe713c2758ecb40dc578944e545ab
wasiakaa <aleksandra.wasiak.stud@pw.edu.pl>
2023-05-19 14:04:25 +0200

    Dodałam szkielet testów jednostkowych

---
+++ Unittests.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    # DSatur
+
+    # def test_dsatur1(self):
+    #     # given
+    #     G1 =
+    #     k =
+    #     expected_result
+
+        # when
+
+        # then
+
+
+# LF
+
+# given
+
+# when
+
+# then
+
+
+# SL
+
+# given
+
+# when
+
+# then
