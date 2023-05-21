5df0ad7f7acbfd1410875c9e617882bd2dbefb4b
Sabina <sabinaduchnowska@gmail.com>
2023-05-21 22:01:14 +0200

    Dodałam 5 testów dla algorytmu Largest First

--- Unittests.py
+++ Unittests.py
@@ -16,7 +16,7 @@ def is_coloring_correct(G, coloring):
 
 
 class MyTestCase(unittest.TestCase):
-    def test_LF(self):
+    def test_largest_first_1(self):
         # given
         G = load_graph_from_file("graph_example_1.txt")
         k = 3
@@ -29,6 +29,76 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(number_of_colors, expected_number_of_colors)  # sprawdzam liczbę użytych kolorów
         self.assertLessEqual(max_repetitions, k)  # sprawdzam liczbę wierzchołków w danym kolorze
         self.assertTrue(is_coloring_correct(G, result))   # sprawdzam poprawność kolorowania
+
+    def test_largest_first_2(self):
+        # given
+        G = load_graph_from_file("graph_example_2.txt")
+        k = 2
+        expected_number_of_colors = 3
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+
+    def test_largest_first_3(self):
+        # given
+        G = load_graph_from_file("graph_example_3.txt")
+        k = 3
+        expected_number_of_colors = 2
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+
+    def test_largest_first_4(self):
+        # given
+        G = load_graph_from_file("graph_example_3.txt")
+        k = 2
+        expected_number_of_colors = 4
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+
+    def test_largest_first_5(self):
+        # given
+        G = load_graph_from_file("graph_example_4.txt")
+        k = 3
+        expected_number_of_colors = 4
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+
+    def test_largest_first_6(self):
+        # given
+        G = load_graph_from_file("graph_example_4.txt")
+        k = 4
+        expected_number_of_colors = 3
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
 
 # DSatur
 
