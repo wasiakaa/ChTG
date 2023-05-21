65d1c5cdf08ae6a65e4dfd14857c5011e18545c3
Sabina <sabinaduchnowska@gmail.com>
2023-05-21 22:39:07 +0200

    Dodałam testy dla algorytmu Smallest Last

--- Unittests.py
+++ Unittests.py
@@ -16,6 +16,7 @@ def is_coloring_correct(G, coloring):
 
 
 class MyTestCase(unittest.TestCase):
+# LARGEST FIRST
     def test_largest_first_1(self):
         # given
         G = load_graph_from_file("graph_example_1.txt")
@@ -100,6 +101,87 @@ class MyTestCase(unittest.TestCase):
         self.assertLessEqual(max_repetitions, k)
         self.assertTrue(is_coloring_correct(G, result))
 
+# SMALLEST LAST
+    def test_smallest_last_1(self):
+        # given
+        G = load_graph_from_file("graph_example_1.txt")
+        k = 3
+        expected_number_of_colors = 3
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+
+    def test_smallest_last_2(self):
+        # given
+        G = load_graph_from_file("graph_example_2.txt")
+        k = 2
+        expected_number_of_colors = 3
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+    def test_smallest_last_3(self):
+        # given
+        G = load_graph_from_file("graph_example_3.txt")
+        k = 3
+        expected_number_of_colors = 2
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+    def test_smallest_last_4(self):
+        # given
+        G = load_graph_from_file("graph_example_3.txt")
+        k = 2
+        expected_number_of_colors = 4
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+    def test_smallest_last_5(self):
+        # given
+        G = load_graph_from_file("graph_example_4.txt")
+        k = 3
+        expected_number_of_colors = 4
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+    def test_smallest_last_6(self):
+        # given
+        G = load_graph_from_file("graph_example_4.txt")
+        k = 4
+        expected_number_of_colors = 3
+        # when
+        result = largest_first(G, k)
+        number_of_colors = max(result)
+        max_repetitions = result.count(mode(result))
+        # then
+        self.assertEqual(number_of_colors, expected_number_of_colors)
+        self.assertLessEqual(max_repetitions, k)
+        self.assertTrue(is_coloring_correct(G, result))
+
 # DSatur
 
 # def test_dsatur1(self):
@@ -107,15 +189,6 @@ class MyTestCase(unittest.TestCase):
 #     G1 =
 #     k =
 #     expected_result
-
-# when
-
-# then
-
-
-# SL
-
-# given
 
 # when
 
